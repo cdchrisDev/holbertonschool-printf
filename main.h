3b268cc418502f4f2428bd8e2a675ce6b8e05215
cdchrisDev <juan.feli.lopez@gmail.com>
2023-12-11 08:58:45 -0800

    first commit

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void _putint(int l);
+int _printf(const char *format, ...);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+#endif
