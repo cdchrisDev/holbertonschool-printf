d673e9c366c57d4ca39eccac029db0e3a88b1ac1
cdchrisDev <juan.feli.lopez@gmail.com>
2023-12-27 18:07:52 -0800

    done

--- main.h
+++ main.h
@@ -1,5 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+typedef struct strFormat
+{
+	char strFrmt = { "%c", "%d", "%e", "%f", "%g", "%i", "%o", "%s", "%u", "%x" "%%" }
+	void (frmtlooup*)(char);
+} strFormat_t;
 
 int _putchar(char c);
 void _putint(int l);
