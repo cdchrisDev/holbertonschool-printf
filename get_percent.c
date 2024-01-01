ff79e40ad377ad18284456f42e1f4f5e2971d960
cdchrisDev <juan.feli.lopez@gmail.com>
2023-12-31 21:19:25 -0800

    spec processor

---
+++ get_percent.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * get_percent - a func to get the spc %
+ * @spc: string specifier
+ * Return: the func
+ */
+int (*Fspc(char spcfier, va_list arg))
+{
+	va_start(arg, format);
+
+	spc_t specs[] = {
+	{"d", printInt},
+	{"i", printInt},
+	{"u", unsigPrnt},
+	{"o", printInt},
+	{"x", printInt},
+	{"X", printInt},
+	{"c", printChr},
+	{"s", printChr},
+	{"p", unsigPrnt},
+	{"r", unsigPrnt},
+	{NULL, NULL}
+	};
+
+	int i = 0;
+	while (specs[i].s != NULL)
+	{
+		if (*specs[i].s == *format && *format != '\0')
+			return (specs[i].Fspc);
+
+		i++;
+	}
+	return (0);
+}
